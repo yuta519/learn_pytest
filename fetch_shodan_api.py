38edad07d83d54aa3216a4ef6d633b0b5c406940
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-18 22:44:30 +0900

    erase unnecessary lines

--- fetch_shodan_api.py
+++ fetch_shodan_api.py
@@ -5,10 +5,8 @@ def fetch_ip_info(ipaddress):
     config = configparser.ConfigParser()
     config.read('config.ini')
     api_key = config['Shodan']['api_key']
-    print(api_key)
     api = Shodan(api_key)
     return type(api.host(ipaddress))
-
 
 if __name__ == '__main__':
     print(fetch_ip_info('8.8.8.8'))
