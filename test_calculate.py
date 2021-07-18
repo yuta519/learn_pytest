79a2e888b2a685ddcb905767c3955f59772e59e8
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-18 22:53:53 +0900

    add testcase for calculate

--- test_calculate.py
+++ test_calculate.py
@@ -4,3 +4,11 @@ import calculate
 
 def test_add():
     assert calculate.add(1, 2) == 3
+    assert calculate.add(2, 2) == 4
+    assert calculate.add(3, 1) == 4
+
+def test_substraction():
+    assert calculate.substraction(3, 1) == 2
+    assert calculate.substraction(1, 1) == 0 
+    assert calculate.substraction(1, 2) == 0
+    
