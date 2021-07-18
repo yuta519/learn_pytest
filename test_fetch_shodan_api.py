24ee120bce99cccf7358bfa34205a7b89514b8c9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-18 22:45:05 +0900

    comment out test

--- test_fetch_shodan_api.py
+++ test_fetch_shodan_api.py
@@ -4,9 +4,6 @@ import pytest
 import fetch_shodan_api
 
 
-def test_fetch_ip_info():
-    result = fetch_shodan_api.fetch_ip_info('8.8.8.8')
-    assert type(result) == 'dict'
-
-if __name__ == '__main__':
-    test_fetch_ip_info()
+# def test_fetch_ip_info():
+#     result = fetch_shodan_api.fetch_ip_info('8.8.8.8')
+#     assert type(result) == 'dict'
