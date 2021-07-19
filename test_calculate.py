c1fbb5688c469b80d85289c6ebf92adbd9cbb9ff
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-19 21:22:47 +0900

    change test func paramternized

--- test_calculate.py
+++ test_calculate.py
@@ -2,13 +2,20 @@ import pytest
 
 import calculate
 
+
+# base test function
 def test_add():
     assert calculate.add(1, 2) == 3
-    assert calculate.add(2, 2) == 4
-    assert calculate.add(3, 1) == 4
 
-def test_substraction():
-    assert calculate.substraction(3, 1) == 2
-    assert calculate.substraction(1, 1) == 0 
-    assert calculate.substraction(1, 2) == 0
+# using decorator for parametered test
+@pytest.mark.parametrize(('number', 'expected'), [
+    (1, 3),
+    (2, 4),
+    (3, 5),
+])
+def test_add2(number: int, expected: int):
+    assert calculate.add(number, 2) == expected
+
+# def test_substraction():
+#     assert calculate.substraction(3, 1) == 2
     
