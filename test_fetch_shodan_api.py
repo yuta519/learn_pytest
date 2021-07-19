1aefd3b1983bd2e84d6dcd7730ee6d7c4cdea0d6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-19 21:23:19 +0900

    add test shodan api file

--- test_fetch_shodan_api.py
+++ test_fetch_shodan_api.py
@@ -4,6 +4,15 @@ import pytest
 import fetch_shodan_api
 
 
-# def test_fetch_ip_info():
-#     result = fetch_shodan_api.fetch_ip_info('8.8.8.8')
-#     assert type(result) == 'dict'
+
+@pytest.mark.parametrize(('ioc', 'key', 'expected'), [
+    ('8.8.8.8', 'region_code', 'CA'),
+    ('8.8.8.8', 'country_code', 'US'),
+    ('8.8.8.8', 'city', 'Mountain View'),
+    ('8.8.8.8', 'ip', 134744072),
+    ('8.8.8.8', 'hostnames', ['dns.google']),
+    ('8.8.8.8', 'org', 'Google LLC'),
+])
+def test_fetch_ip_info(ioc, key, expected):
+    result = fetch_shodan_api.fetch_ip_info(ioc)
+    assert result[key] == expected
