d9ad9de26a1cf8ab5017e5ccbc801c4c23c4d729
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-19 21:23:02 +0900

    add test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,15 @@
+import pytest
+
+# def func(x):
+#     return x + 1
+
+# def test_answer():
+#     assert func(3) == 5
+
+
+def f():
+    raise SystemExit(1)
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
