20944b57d88da5edb9558a44b17047ec9812322b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-19 22:43:29 +0900

    add test case

--- test_fetch_shodan_api.py
+++ test_fetch_shodan_api.py
@@ -1,8 +1,6 @@
-import requests
 import pytest
 
 import fetch_shodan_api
-
 
 
 @pytest.mark.parametrize(('ioc', 'key', 'expected'), [
@@ -13,6 +11,18 @@ import fetch_shodan_api
     ('8.8.8.8', 'hostnames', ['dns.google']),
     ('8.8.8.8', 'org', 'Google LLC'),
 ])
-def test_fetch_ip_info(ioc, key, expected):
+def test_fetch_ip_googledns(ioc, key, expected):
     result = fetch_shodan_api.fetch_ip_info(ioc)
     assert result[key] == expected
+
+@pytest.mark.parametrize(('ioc', 'key'), [
+    ('1.1.1.1', 'region_code'),
+    ('1.1.1.1', 'country_code'),
+    ('1.1.1.1', 'city'),
+    ('1.1.1.1', 'ip'),
+    ('1.1.1.1', 'hostnames'),
+    ('1.1.1.1', 'org'),
+])
+def test_fetch_ip_base(ioc, key):
+   result = fetch_shodan_api.fetch_ip_info(ioc) 
+   assert key in result.keys()
