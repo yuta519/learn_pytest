0a7f75fd3d8bebd7b64129b3f2123c9f3d400b91
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-19 21:22:13 +0900

    change shodan api result

--- fetch_shodan_api.py
+++ fetch_shodan_api.py
@@ -6,7 +6,7 @@ def fetch_ip_info(ipaddress):
     config.read('config.ini')
     api_key = config['Shodan']['api_key']
     api = Shodan(api_key)
-    return type(api.host(ipaddress))
+    return api.host(ipaddress)
 
 if __name__ == '__main__':
     print(fetch_ip_info('8.8.8.8'))
