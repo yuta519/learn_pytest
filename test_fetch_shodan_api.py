b4e23c6783faf56c6406c1adb3e2eac9a0717339
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-22 21:50:47 +0900

    change test parameters

--- test_fetch_shodan_api.py
+++ test_fetch_shodan_api.py
@@ -15,14 +15,11 @@ def test_fetch_ip_googledns(ioc, key, expected):
     result = fetch_shodan_api.fetch_ip_info(ioc)
     assert result[key] == expected
 
-@pytest.mark.parametrize(('ioc', 'key'), [
-    ('1.1.1.1', 'region_code'),
-    ('1.1.1.1', 'country_code'),
-    ('1.1.1.1', 'city'),
-    ('1.1.1.1', 'ip'),
-    ('1.1.1.1', 'hostnames'),
-    ('1.1.1.1', 'org'),
+@pytest.mark.parametrize(('ioc', 'keys'), [
+    ('1.1.1.1', ['region_code','country_code','city','ip','hostnames','org'])
 ])
-def test_fetch_ip_base(ioc, key):
-   result = fetch_shodan_api.fetch_ip_info(ioc) 
-   assert key in result.keys()
+def test_fetch_ip_base(ioc, keys) -> None:
+    response = fetch_shodan_api.fetch_ip_info(ioc) 
+    for key in keys:
+        print(key)
+        assert key in response.keys()
