3ec45474409d404cfe7768602c729ff3ece18464
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-23 23:02:32 +0900

    add fetch_ip function using rest api

--- fetch_shodan_api.py
+++ fetch_shodan_api.py
@@ -1,5 +1,17 @@
 import configparser
+
+import requests
 from shodan import Shodan
+
+
+def fetch_ip(ipaddress):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    api_key = config['Shodan']['api_key']
+    endpoint = f'https://api.shodan.io/shodan/host/{ipaddress}?key={api_key}'
+    response = requests.get(endpoint)
+    return response.json()
+
 
 def fetch_ip_info(ipaddress):
     config = configparser.ConfigParser()
@@ -9,4 +21,4 @@ def fetch_ip_info(ipaddress):
     return api.host(ipaddress)
 
 if __name__ == '__main__':
-    print(fetch_ip_info('8.8.8.8'))
+    print(fetch_ip('8.8.8.8'))
