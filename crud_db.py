34f3550b8834f35a749dd0ab1c111571927c28db
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-25 22:28:07 +0900

    create session to postgres

--- crud_db.py
+++ crud_db.py
@@ -1,4 +1,15 @@
+import psycopg2
 from sqlalchemy import create_engine
 
-def create_db():
-    pass
+def get_connection():
+    # dsn = (f"postgresql+psycopg2://postgres:@127.0.0.1:5433/postgres")
+    dsn = ("dbname=postgres  user=postgres password= host=localhost port=5433")
+    return psycopg2.connect(dsn) 
+
+
+if __name__ == '__main__':
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users')
+    cur.close()
+    conn.close()
