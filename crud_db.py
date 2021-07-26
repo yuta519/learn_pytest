8df6674f935991b4442c8496c988a91f17ab1fd5
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-26 22:38:25 +0900

    create sql session and execute select query

--- crud_db.py
+++ crud_db.py
@@ -1,15 +1,26 @@
 import psycopg2
+
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 def get_connection():
-    # dsn = (f"postgresql+psycopg2://postgres:@127.0.0.1:5433/postgres")
-    dsn = ("dbname=postgres  user=postgres password= host=localhost port=5433")
+    # dsn = "postgresql+psycopg2://yuta519:@127.0.0.1:5433/tests"
+    # dsn = "postgresql+psycopg2://yuta519:@127.0.0.1:5433/tests"
+    dsn = ("dbname=tests  user=yuta519 password= host=localhost port=5433")
+    # engine = create_engine(dsn)
+
+    # Create test database and tables
+    # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+ 
     return psycopg2.connect(dsn) 
 
 
 if __name__ == '__main__':
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM users')
+    cur.execute('SELECT * FROM mybook')
+    rows = cur.fetchall()
     cur.close()
     conn.close()
+    print(rows)
