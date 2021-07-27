ae3f266802dd489fb0f5d6ef8fbe6f80396a64f0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-27 22:56:39 +0900

    create global value for shodan api url

--- fetch_shodan_api.py
+++ fetch_shodan_api.py
@@ -3,12 +3,13 @@ import configparser
 import requests
 from shodan import Shodan
 
+api_base_url = 'https://api.shodan.io'
 
 def fetch_ip(ipaddress):
     config = configparser.ConfigParser()
     config.read('config.ini')
     api_key = config['Shodan']['api_key']
-    endpoint = f'https://api.shodan.io/shodan/host/{ipaddress}?key={api_key}'
+    endpoint = f'{api_base_url}/shodan/host/{ipaddress}?key={api_key}'
     response = requests.get(endpoint)
     return response.json()
 
