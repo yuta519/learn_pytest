da3095d257088c8d03f8f92644460b058ea98a22
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-30 23:34:52 +0900

    create base api key function

--- fetch_shodan_api.py
+++ fetch_shodan_api.py
@@ -5,21 +5,30 @@ from shodan import Shodan
 
 api_base_url = 'https://api.shodan.io'
 
-def fetch_ip(ipaddress):
+
+def return_api_key():
     config = configparser.ConfigParser()
     config.read('config.ini')
     api_key = config['Shodan']['api_key']
+    return api_key
+
+def fetch_ip(ipaddress):
+    api_key = return_api_key()
     endpoint = f'{api_base_url}/shodan/host/{ipaddress}?key={api_key}'
     response = requests.get(endpoint)
     return response.json()
 
 
 def fetch_ip_info(ipaddress):
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    api_key = config['Shodan']['api_key']
+    api_key = return_api_key()
     api = Shodan(api_key)
     return api.host(ipaddress)
 
+def fetch_notifier():
+    pass
+
+
+
 if __name__ == '__main__':
     print(fetch_ip('8.8.8.8'))
+    print(fetch_ip_info('8.8.8.8'))
