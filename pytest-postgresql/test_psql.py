90445c15e924d87e0254233faa1be5110b9923f4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-08-23 22:27:48 +0900

    delete comments

--- pytest-postgresql/test_psql.py
+++ pytest-postgresql/test_psql.py
@@ -21,23 +21,9 @@ def test_insert_user_data(setup_database: Session) -> None:
     users = setup_database.query(User).filter(User.user_name=="Yuta").all()
     for user in users:
         assert user.user_name == "Yuta"
-    
 
 
-
-
-def test_user_data(insert_data) -> None:
-    # try:
-    #     user = User(
-    #         user_name="Yuta",
-    #         user_age=28
-    #     )
-    #     setup_database.add(user)
-    #     setup_database.commit()
-    # except RuntimeError:
-    #     print(RuntimeError)
-    #     setup_database.rollback()
-
-    users = insert_data.query(User).filter(User.user_name=="Yuta").all()
+def test_user_data(testdata_for_root: Session) -> None:
+    users = testdata_for_root.query(User).filter(User.user_name=="Yuta").all()
     for user in users:
         assert user.user_name == "Yuta"
